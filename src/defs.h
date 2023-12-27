339d308e568c7b9071d93bd2c004b960ba95b9b3
Joseph <49760581+jmnc2@users.noreply.github.com>
2023-12-27 01:04:46 +0000

    added pro micro #defines

--- src/defs.h
+++ src/defs.h
@@ -1,57 +1,83 @@
 /**
  * @file defs.h
  * @author jmnc2 (you@domain.com)
- * @brief 
+ * @brief
  * @version 0.0.4
  * @date 2022-03-03
- * 
+ *
  * @copyright Copyright (c) 2022
- * 
+ *
  */
 #ifndef _defs_h
 #define _defs_h
 
 #include <Arduino.h>
-//#include "UI_IO.h"
+// #include "UI_IO.h"
 
-//Menu_C menu();
-// MCU used __AVR_ATmega328P__
+// Menu_C menu();
+//  MCU used __AVR_ATmega328P__
 #ifdef __AVR_ATmega328P__
- #define ATmega328P_mcu
- #define ATmega328P
- #define  mem_eeprom 1024
- #define  mem_flash 32256
- #define mem_sram 2048
- #define _pwmPins {3, 5, 6, 9, 10, 11}//timer interferes with 5 & 6
- #define InOnlyPins = 2 // ATmega328P chips have 2 analog input only pins namely A6 & A7. Some older boards do not bring them out to pins but most do.
-#endif  
-//#define MCU_type nano_mcu
+#define ATmega328P_mcu
+#define ATmega328P
+#define mem_eeprom 1024
+#define mem_flash 32256
+#define mem_sram 2048
+#define _pwmPins       \
+  {                    \
+    3, 5, 6, 9, 10, 11 \
+  }                    // timer interferes with 5 & 6
+#define InOnlyPins = 2 // ATmega328P chips have 2 analog input only pins namely A6 & A7. Some older boards do not bring them out to pins but most do.
+#endif
+
+#ifdef __AVR_ATmega32U4__
+#define ATmega32U4_mcu
+#define ATmega32U4
+#define mem_eeprom 1024
+#define mem_flash 32256
+#define mem_sram 2560
+#define _pwmPins           \
+  {                        \
+    3, 5, 6, 9, 10, 11 \  
+   \
+  } // Pro Micro don't have pins 11,12,13 brocken out.
+
+// timer interferes with 5 & 6
+#define InOnlyPins = 0 // ATmega328P chips have 2 analog input only pins namely A6 & A7. Some older boards do not bring them out to pins but most do.
+#endif
+// #define MCU_type nano_mcu
 
 // Arduino board types
-#define unknown_board  0
-#define Uno_board  1
+#define unknown_board 0
+#define Uno_board 1
 #define Nano_board 2
 
 #if defined(ARDUINO_AVR_NANO)
-  #define _Nano
-  #define board_name "Nano"
-  #define board_type Nano_board
+#define _Nano
+#define board_name "Nano"
+#define board_type Nano_board
 #elif defined(ARDUINO_AVR_UNO)
-  #define _Uno
-  #define board_type Uno_board
-  #define board_name "Uno"
+#define _Uno
+#define board_type Uno_board
+#define board_name "Uno"
+#elif defined(ARDUINO_AVR_PROMICRO16)
+#define _Micro
+#define board_name "Pro Micro"
+#define board_type Nano_board
+
 #else
-   #define _pwmPins {0xff} //using the max value with array size of 1 only to signify that there are no pwm pins.
-   #define board_type unknown_board 
-#endif  
+#define _pwmPins \
+  {              \
+    0xff         \
+  } // using the max value with array size of 1 only to signify that there are no pwm pins.
+#define board_type unknown_board
+#endif
 
-
-extern byte pwm_pins[];// = _pwmPins;
+extern byte pwm_pins[]; // = _pwmPins;
 // an empty array for arrays where the first element is the array size.
 extern byte eA[];
 
 // User output type
-#define termIO //output to the Serial Monitor
+#define termIO     // output to the Serial Monitor
 #define screenIO 0 // Small screen. i.e. Nokia 5110.
 #define lcd16x2 0
 #define lcd20x4 0
@@ -118,9 +144,9 @@ extern byte eA[];
 #endif
 
 // The type of Input the switch is on. e.g. Local mcu GPIO pin, 1wire, IO extender, shift register etc.
-#define ct_pin 0    //ct stands for controller type
+#define ct_pin 0 // ct stands for controller type
 #define ct_1wire 1
-#define ct_I2C 2    // this should probably be more specific as in a type of i2c IO expander.
+#define ct_I2C 2 // this should probably be more specific as in a type of i2c IO expander.
 #define ct_SPI 3
 #define ct_array 4
 #define ioLocalPin 0
@@ -129,10 +155,10 @@ extern byte eA[];
 /**
  * Override the defs in the unit headers with values for this project.
  */
-//#define t 1
+// #define t 1
 
 boolean is_pwmPin(byte p);
-byte cpBitsMsk8(byte a, byte b, byte msk);//set bit in a matched by msk to value(s) from b
+byte cpBitsMsk8(byte a, byte b, byte msk); // set bit in a matched by msk to value(s) from b
 
 ;
 #endif
