4a2898fae2f4a1b46b6f3d10e30df3ef7fa43189
Joseph <49760581+jmnc2@users.noreply.github.com>
2024-07-10 14:07:42 +0100

    added pro Micro board

--- src/defs.h
+++ src/defs.h
@@ -21,12 +21,12 @@
 #define mem_eeprom 1024
 #define mem_flash 32256
 #define mem_sram 2048
-#define _pwmPins       \
-  {                    \
-    3, 5, 6, 9, 10, 11 \
-  }
+#define _pwmPins                                                               \
+  { 3, 5, 6, 9, 10, 11 }
 // timer interferes with 5 & 6
-#define InOnlyPins = 2 // ATmega328P chips have 2 analog input only pins namely A6 & A7. Some older boards do not bring them out to pins but most do.
+#define InOnlyPins                                                             \
+  = 2 // ATmega328P chips have 2 analog input only pins namely A6 & A7. Some
+      // older boards do not bring them out to pins but most do.
 #endif
 
 #ifdef __AVR_ATmega32U4__
@@ -35,14 +35,13 @@
 #define mem_eeprom 1024
 #define mem_flash 32256
 #define mem_sram 2560
-#define _pwmPins           \
-  {                        \
-    3, 5, 6, 9, 10, 11     \
-      \
-  } // Pro Micro don't have pins 11,12,13 brocken out.
+#define _pwmPins                                                               \
+  { 3, 5, 6, 9, 10, 11 } // Pro Micro don't have pins 11,12,13 brocken out.
 
 // timer interferes with 5 & 6
-#define InOnlyPins = 0 // ATmega328P chips have 2 analog input only pins namely A6 & A7. Some older boards do not bring them out to pins but most do.
+#define InOnlyPins                                                             \
+  = 0 // ATmega328P chips have 2 analog input only pins namely A6 & A7. Some
+      // older boards do not bring them out to pins but most do.
 #endif
 // #define MCU_type nano_mcu
 
@@ -50,6 +49,7 @@
 #define unknown_board 0
 #define Uno_board 1
 #define Nano_board 2
+#define Pro_Micro_board 3
 
 #if defined(ARDUINO_AVR_NANO)
 #define _Nano
@@ -62,13 +62,13 @@
 #elif defined(ARDUINO_AVR_PROMICRO16)
 #define _Micro
 #define board_name "Pro Micro"
-#define board_type Nano_board
-
+#define board_type Pro_Micro_board
 #else
-#define _pwmPins \
-  {              \
-    0xff         \
-  } // using the max value with array size of 1 only to signify that there are no pwm pins.
+#define _pwmPins                                                               \
+  {                                                                            \
+    0xff                                                                       \
+  } // using the max value with array size of 1 only to signify that there are
+    // no pwm pins.
 #define board_type unknown_board
 #endif
 
@@ -143,10 +143,12 @@ extern byte eA[];
 #define D19 PIN_D19
 #endif
 
-// The type of Input the switch is on. e.g. Local mcu GPIO pin, 1wire, IO extender, shift register etc.
+// The type of Input the switch is on. e.g. Local mcu GPIO pin, 1wire, IO
+// extender, shift register etc.
 #define ct_pin 0 // ct stands for controller type
 #define ct_1wire 1
-#define ct_I2C 2 // this should probably be more specific as in a type of i2c IO expander.
+#define ct_I2C                                                                 \
+  2 // this should probably be more specific as in a type of i2c IO expander.
 #define ct_SPI 3
 #define ct_array 4
 #define ioLocalPin 0
@@ -158,7 +160,8 @@ extern byte eA[];
 // #define t 1
 
 boolean is_pwmPin(byte p);
-byte cpBitsMsk8(byte a, byte b, byte msk); // set bit in a matched by msk to value(s) from b
+byte cpBitsMsk8(byte a, byte b,
+                byte msk); // set bit in a matched by msk to value(s) from b
 
 ;
 #endif
